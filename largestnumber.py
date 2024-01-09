4ba5c681b23cc547cda31d5abc291d39da6b6c9e
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-09 17:00:28 +0800

    printing of the largest number

--- largestnumber.py
+++ largestnumber.py
@@ -10,5 +10,12 @@ num2 = (input("Enter second number: "))
 num3 = (input("Enter third number: "))
 
 #Compare the 3 given numbers
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+if num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
 
 #Print the largest number
+print("The largest number is:", largest)
