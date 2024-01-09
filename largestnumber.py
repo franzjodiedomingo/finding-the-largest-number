4ea2176c2787423a28d99a5a204d88709baf143f
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-09 19:16:20 +0800

    value error

--- largestnumber.py
+++ largestnumber.py
@@ -1,15 +1,26 @@
 #Ask input from the user
 
-#Enter the first number
-num1 = (input("Enter first number: "))
+while True:
+    try: 
+        #Enter the first number
+        num1 = int(input("Enter first number: "))
 
-#Enter the second number
-num2 = (input("Enter second number: "))
+        #Enter the second number
+        num2 = int(input("Enter second number: "))
 
-#Enter the third number
-num3 = (input("Enter third number: "))
+        #Enter the third number
+        num3 = int(input("Enter third number: "))
 
-#Compare the 3 given numbers
+        #Break out of the loop if all the inputs are successful
+        break
+
+    except ValueError: 
+        print ("Please enter a valid number.")
+
+# Create a list of the numbers
+numbers = [num1, num2, num3]
+
+ #Compare the 3 given numbers
 if num1 >= num2 and num1 >= num3:
     largest = num1
 elif num2 >= num1 and num2 >= num3:
@@ -18,7 +29,7 @@ else:
     largest = num3
 
 #Print the largest number
-print("The largest number is: ", largest)
+print("The largest number is:", largest)
 
 # Create a list of the numbers
 numbers = [num1, num2, num3]
