18b6160d75d7773cfa746bb2abc408a0f52520d8
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-09 17:48:37 +0800

    fixing the error

--- largestnumber.py
+++ largestnumber.py
@@ -12,7 +12,7 @@ num3 = (input("Enter third number: "))
 #Compare the 3 given numbers
 if num1 >= num2 and num1 >= num3:
     largest = num1
-if num2 >= num1 and num2 >= num3:
+elif num2 >= num1 and num2 >= num3:
     largest = num2
 else:
     largest = num3
