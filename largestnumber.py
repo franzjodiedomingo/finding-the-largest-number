e69d6f76fb6d020fff820852efddd0599ae34293
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-10 18:12:16 +0800

    variables

--- largestnumber.py
+++ largestnumber.py
@@ -23,7 +23,7 @@ numbers = [num1, num2, num3]
  #Compare the 3 given numbers
 if num1 >= num2 and num1 >= num3:
     largest = num1
-elif num2 >= num1 and num2 >= num3:
+if num2 >= num1 and num2 >= num3:
     largest = num2
 else:
     largest = num3
